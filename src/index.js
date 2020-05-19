2dba35c6f7c9ec1430f2557d71d0dacce60c65b6
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-19 09:52:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+         event.preventDefault();
+}, false);
